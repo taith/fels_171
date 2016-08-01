8fd34645ebd13d57176ba7840d01a7788b55f17a
vietmt <mai.tuan.viet@framgia.com>
2016-08-01 13:55:22 +0700

    Initial commit

---
+++ src/framgiavn/project01/web/dao/UserDAO.java
@@ -0,0 +1,11 @@
+package framgiavn.project01.web.dao;
+
+import framgiavn.project01.web.model.User;
+
+public interface UserDAO {
+	public User findByUserId(Integer user_id) throws Exception;
+
+	public User findByUsername(String username) throws Exception;
+	
+	public User findByEmail(String email) throws Exception;
+}
---
+++ src/framgiavn/project01/web/model/User.java
@@ -0,0 +1,77 @@
+package framgiavn.project01.web.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private int user_id;
+	private String email;
+	private String name;
+	private String avatar;
+	private String password;
+	private boolean enabled;
+	private Date created_at;
+	private Date updated_at;
+	
+	
+	
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAvatar() {
+		return avatar;
+	}
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public Date getCreated_at() {
+		return created_at;
+	}
+	public void setCreated_at(Date created_at) {
+		this.created_at = created_at;
+	}
+	public Date getUpdated_at() {
+		return updated_at;
+	}
+	public void setUpdated_at(Date updated_at) {
+		this.updated_at = updated_at;
+	}
+	
+	
+
+
+}
